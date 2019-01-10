86e788d3f63994b8a98df1345b5be567f8658793
Ronildo <ronildo.ufpi@gmail.com>
2019-01-10 10:11:46 -0300

    Envio das rotas do site

--- site.php
+++ site.php
@@ -1,4 +1,19 @@
 <?php
 
+use \Click\Page;
+use \Click\Model\Product;
+use \Click\Model\User;
+
+$app->get('/', function() {
+    
+	$products = Product::listAll();
+
+    $page = new Page();
+	$page->setTpl("index",[
+
+		'products'=>Product::checklist($products)
+	]);
+
+});
 
 ?>
