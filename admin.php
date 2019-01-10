e6b5584d9b2143df582194c34aeca424b987b85e
arturbr97 <arturicozeira@hotmail.com>
2019-01-10 11:52:35 -0200

    Rotas para realizar o login e logout

--- admin.php
+++ admin.php
@@ -12,5 +12,42 @@ $app->get('/admin', function() {
 	$page->setTpl("index");
 
 });
+$app->get('/admin/login', function() {
 
+  $page = new PageAdmin([
+    "header"=>false,
+    "footer"=>false
+  ]);
+
+  $page->setTpl("login",[
+
+    'error'=>User::getError(),
+    'errorRegister'=>User::getErrorRegister(),
+    'registerValues'=>(isset($_SESSION['registerValues'])) ? $_SESSION['registerValues']: ['name'=>'', 'email'=>'','phone'=>'']
+
+  ]);
+
+});
+
+$app->post('/admin/login', function() {
+
+try{
+  User::login($_POST["login"], $_POST["password"]);
+}catch(Exception $e){
+
+    User::setError($e->getMessage());
+  }
+  header("Location: /admin");
+  exit;
+
+});
+
+$app->get('/admin/logout', function() {
+    
+   User::logout();
+
+   header("Location: /admin/login");
+   exit;
+
+});
 ?>
