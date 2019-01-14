e22d50bfbe77201549c8c7c71c6861dea02d2ca7
tonifc007 <antonio007023@gmail.com>
2019-01-14 15:25:54 -0200

    Complemento de functions

--- functions.php
+++ functions.php
@@ -1,13 +1,37 @@
 <?php 
 
+use \Click\Model\Cart;
 
-function formatPrice($vlprice)
-{
+function formatPrice($vlprice){
 
 	if (!$vlprice > 0) $vlprice = 0;
 
 	return number_format($vlprice, 2, ",", ".");
 
 }
+
+// Inicio Commit
+
+function getCartNrQtd(){
+
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return $totals['nrqtd'];
+
+}
+
+function getCartVlSubTotal(){
+
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return formatPrice($totals['vlprice']);
+
+}
+
+//Fim Commit
 
  ?>
--- site.php
+++ site.php
@@ -3,6 +3,8 @@
 use \Click\Page;
 use \Click\Model\Product;
 use \Click\Model\User;
+use \Click\Model\Category;
+use \Click\Model\Cart;
 
 $app->get('/', function() {
     
@@ -62,5 +64,76 @@ $app->get("/products/:desurl", function($desurl){
 
 
 });
+
+//Inicio Commit Carinho
+$app->get("/cart", function(){
+
+	$cart = Cart::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("cart",[
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
+
+	]);
+
+});
+
+$app->get("/cart/:idproduct/add", function($idproduct){
+
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
+
+	for ($i=0;$i<$qtd;$i++){
+
+			$cart->addProduct($product);
+
+	}
+
+
+	header("Location: /cart");
+	exit;
+
+});
+
+$app->get("/cart/:idproduct/minus", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product);
+
+	header("Location: /cart");
+	exit;
+
+});
+
+$app->get("/cart/:idproduct/remove", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
+
+});
+//Fim Commit
+
 
 ?>
